f24d3b6e5243cc4d3c8cec0348f61b4d3cc6dd4f
Kevin Romano <kromano1@bmt.local>
2018-08-06 11:18:36 -0500

    Fixed typo git-pip to get-pip

--- setup_wargame.sh
+++ setup_wargame.sh
@@ -53,7 +53,7 @@ touch .hushlogin
 yum update
 yum install -y nano git gdb gcc gcc-c++ git ltrace kernel-devel openssh-server python python-devel vim guile guile-devel unzip socat ncurses ncurses-devel texinfo make
 curl "https://bootstrap.pypa.io/get-pip.py" -o get-pip.py
-python git-pip.py
+python get-pip.py
 
 #sudo dpkg-reconfigure -plow unattended-upgrades
 
